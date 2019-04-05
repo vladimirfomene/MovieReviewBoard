4a9709b147228801ca4942daab41b645832d1d58
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-05 11:12:30 +0100

    bootstrap Spring Boot app from start.spring.io

---
+++ backend/src/main/kotlin/com/auth0/MovieReviewBoard/MovieReviewBoardApplication.kt
@@ -0,0 +1,11 @@
+package com.auth0.MovieReviewBoard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieReviewBoardApplication
+
+fun main(args: Array<String>) {
+	runApplication<MovieReviewBoardApplication>(*args)
+}
