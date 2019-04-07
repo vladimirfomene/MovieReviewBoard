f7df3f46d7ccf1a0948972e5c5bb264313509659
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-07 18:52:43 +0100

    create query and mutation resolvers

---
+++ backend/src/main/kotlin/com/auth0/MovieReviewBoard/resolver/Mutation.kt
@@ -0,0 +1,50 @@
+package com.auth0.MovieReviewBoard.resolver
+
+import com.auth0.MovieReviewBoard.director.Director
+import com.auth0.MovieReviewBoard.director.DirectorRepository
+import com.auth0.MovieReviewBoard.movie.Movie
+import com.auth0.MovieReviewBoard.movie.MovieRepository
+import com.coxautodev.graphql.tools.GraphQLMutationResolver
+import org.springframework.stereotype.Component
+
+@Component
+class Mutation(val directorRepository: DirectorRepository, val movieRepository: MovieRepository) : GraphQLMutationResolver{
+
+    fun updateDirector(directorId: Long, director: Director): Director {
+
+        val oldDirector = directorRepository.findById(directorId)
+
+        oldDirector.ifPresent{
+            it.firstName = director.firstName
+            it.lastName = director.lastName
+        }
+
+
+        return oldDirector.get()
+    }
+
+    fun updateMovieRating(movieId: Long, vote: Long): Long {
+        val movie = movieRepository.findById(movieId)
+
+        movie.ifPresent{
+            it.rating = it.rating + vote
+            movieRepository.save(it)
+        }
+
+        return movie.get().rating
+
+    }
+
+    fun newMovie(title: String, directorID: Long, releaseDate: String, rating: Long) : Movie{
+        val director = directorRepository.findById(directorID)
+        val movie = Movie(title, director.get(), rating, releaseDate)
+
+        return movieRepository.save(movie)
+    }
+
+    fun newDirector(directorId: Long, firstName: String, lastName: String): Director {
+        val director = Director(firstName, lastName, directorId)
+
+        return directorRepository.save(director)
+    }
+}
---
+++ backend/src/main/kotlin/com/auth0/MovieReviewBoard/resolver/Query.kt
@@ -0,0 +1,38 @@
+package com.auth0.MovieReviewBoard.resolver
+
+import com.auth0.MovieReviewBoard.director.Director
+import com.auth0.MovieReviewBoard.director.DirectorRepository
+import com.auth0.MovieReviewBoard.movie.Movie
+import com.auth0.MovieReviewBoard.movie.MovieRepository
+import com.coxautodev.graphql.tools.GraphQLQueryResolver
+import org.springframework.stereotype.Component
+
+@Component
+class Query (val movieRepository: MovieRepository, val directorRepository: DirectorRepository) : GraphQLQueryResolver{
+
+    fun findAllMovies(): Iterable<Movie>{
+        return movieRepository.findAll()
+    }
+
+    fun findAllDirectors(): Iterable<Director>{
+        return directorRepository.findAll()
+    }
+
+    fun countMovies(): Long{
+        return movieRepository.count()
+    }
+
+    fun countDirectors(): Long{
+        return directorRepository.count()
+    }
+
+    fun findMoviesByDirector(directorId: Long): Iterable<Movie>{
+        val director = directorRepository.findById(directorId)
+        return movieRepository.findByDirector(director.get())
+    }
+
+    fun countMoviesByDirector(directorId: Long): Long{
+        val director = directorRepository.findById(directorId)
+        return movieRepository.countByDirector(director.get())
+    }
+}
