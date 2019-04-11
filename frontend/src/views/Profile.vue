8963dd9b2f6134b4b7c22f029a6bb28cf7caaa04
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-11 12:37:54 +0100

    create authentication service using Auth0

---
+++ frontend/src/views/Profile.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="profile">
+		<NavBarComponent v-bind:brand-title="brandTitle" />
+    <div>
+      <div>
+        <img :src="profile.picture">
+      </div>
+      <div>
+        <h2>{{ profile.name }}</h2>
+        <p>{{ profile.email }}</p>
+      </div>
+    </div>
+
+    <div>
+      <pre>{{ JSON.stringify(profile, null, 2) }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBarComponent from "@/components/NavBarComponent.vue";
+
+export default {
+	name: "profile",
+	components: {
+		NavBarComponent
+	},
+  data() {
+    return {
+			profile: this.$auth.profile,
+			brandTitle: "Movie Review Board"
+    };
+  },
+  methods: {
+    handleLoginEvent(data) {
+      this.profile = data.profile;
+    }
+	}
+};
+</script>
