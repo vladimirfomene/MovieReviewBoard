449b1f34873732c3d8d55352ad5edb98100922ce
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-11 13:05:47 +0100

    remove bootstrapped navigation from root view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
+<script>
+export default {
+	name: "app",
+	async created() {
+    try {
+      await this.$auth.renewTokens();
+    } catch {
+      // Supress the 'not logged in' error as we can illegitimately get that
+      // when processing the callback url
+    }
+  }
+}
+</script>
+
+
 <style>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto+Slab');
+
+html{
+	height: 100%;
+}
+body {
+	background: #D3CCE3;  /* fallback for old browsers */
+	background: -webkit-linear-gradient(to top, #E9E4F0, #D3CCE3);  /* Chrome 10-25, Safari 5.1-6 */
+	background: linear-gradient(to top, #E9E4F0, #D3CCE3); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+	background-repeat: no-repeat;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
