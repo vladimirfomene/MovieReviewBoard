947999524077a634c36510e9fee903d2deb2caa5
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-11 13:04:19 +0100

    create app wide nav bar

---
+++ frontend/src/components/NavBarComponent.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="nav">
+		<nav class="navbar navbar-dark bg-primary" id="navbar">
+			<a class="navbar-brand" id="brand-title">{{ brandTitle }}</a>
+			<ul class="nav justify-content-end">
+				<li class="nav-item">
+					<router-link to="/profile" class="nav-link">Profile</router-link>
+				</li>
+				<li class="nav-item">
+					<button @click="logout">Logout</button>
+				</li>
+		</ul>
+		</nav>
+	</div>
+</template>
+
+
+<script>
+export default {
+	name: "navbarcomponent",
+	props: {
+		brandTitle: String
+	},
+	methods: {
+		logout(){
+			this.$auth.logOut();
+		}
+	}
+}
+</script>
+
+
+<style scoped>
+#brand-title{
+	font-family: 'Roboto Slab', serif;
+}
+#navbar{
+	width: 100%;
+}
+</style>
+
+
+
