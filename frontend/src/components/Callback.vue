20b269206b63148dcee6aa4bdd4b3d161440082d
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-11 12:40:44 +0100

    create a callback view that will be called by Auth0 on success auth

---
+++ frontend/src/components/Callback.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="spinner-border text-primary" role="status">
+  	<span class="sr-only">Loading...</span>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleLoginEvent(data) {
+      this.$router.push(data.state.target || "/movieboard");
+    }
+  },
+  created() {
+    this.$auth.handleAuthentication();
+  }
+};
+</script>
+
+
+<style scoped>
+div.spinner-border{
+	margin: 10% auto;
+}
+</style>
