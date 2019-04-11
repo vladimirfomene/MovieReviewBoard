acd8c9f32f68b9db0aeb17eb0959b43a5a37f790
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-11 12:36:35 +0100

    create view to display list of movies

---
+++ frontend/src/views/MovieBoard.vue
@@ -0,0 +1,28 @@
+<template>
+	<NavBarComponent v-bind:brand-title="brandTitle" />
+</template>
+
+
+<script>
+
+import NavBarComponent from "@/components/NavBarComponent.vue";
+
+export default {
+	name: "movieboard",
+	components: {
+		NavBarComponent
+	},
+	data(){
+		return {
+			brandTitle: "Movie Review Board"
+		};
+	}
+}
+</script>
+
+
+<style scoped>
+
+</style>
+
+
