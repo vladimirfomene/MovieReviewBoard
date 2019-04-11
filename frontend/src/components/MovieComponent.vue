ae9a3360c2c2fe44c6841e7defc64cc92d74a63e
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-11 13:04:55 +0100

    create component for movie entities

---
+++ frontend/src/components/MovieComponent.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="card" style="width: 18rem;" v-for="movie in movies" :key="movie.id">
+		<div class="card-body">
+			<h5 class="card-title">Title: {{ movie.title }}</h5>
+			<p class="card-text">Rating: {{ movie.rating }}</p>
+			<a href="#" class="card-link">Release Date: {{ movie.releaseDate }}</a>
+			<a href="#" class="card-link">Director: {{ movie.director }}</a>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "moviecomponent",
+	computed: {
+		
+	},
+	created(){
+		this.$store.dispatch("getMovies", );
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
