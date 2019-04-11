b89df58bb91450ff158c4c028640bffe3b766f65
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-11 13:03:46 +0100

    create login component for homepage

---
+++ frontend/src/components/LoginComponent.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="card" id="login-card">
+		<div class="card-body">
+			<p class="card-text" id="welcome-text">Sign in to start rating your favorite movies.</p>
+			<a href="#" class="btn btn-primary" id="login-btn" @click.prevent="login">Login Here</a>
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: "logincomponent",
+	methods: {
+		login(){
+			this.$auth.login();
+		}
+	}
+}
+</script>
+
+
+<style scoped>
+#login-btn{
+	font-family: 'Open Sans', sans-serif;
+}
+#welcome-text{
+	font-family: 'Roboto Slab', serif;
+}
+#login-card{
+	max-width: 50%;
+	margin: 10% auto;
+}
+</style>
+
+
