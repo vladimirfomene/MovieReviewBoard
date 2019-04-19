0e2ded95ffc86529b27fc2ec7836482adfaa0263
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-19 19:37:02 +0100

    add font awesome icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
 import "bootstrap/dist/css/bootstrap.min.css";
-import AuthPlugin from "./plugins/auth";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-Vue.use(AuthPlugin);
+library.add(faAngleDown, faAngleUp);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
