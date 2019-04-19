0b3468c0a37315c81d8e0fea1e93376636a78bea
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-19 19:39:05 +0100

    create a component to display a movie's details

--- frontend/src/components/MovieComponent.vue
+++ frontend/src/components/MovieComponent.vue
@@ -1,28 +1,61 @@
 <template>
-	<div class="card" style="width: 18rem;" v-for="movie in movies" :key="movie.id">
-		<div class="card-body">
-			<h5 class="card-title">Title: {{ movie.title }}</h5>
-			<p class="card-text">Rating: {{ movie.rating }}</p>
-			<a href="#" class="card-link">Release Date: {{ movie.releaseDate }}</a>
-			<a href="#" class="card-link">Director: {{ movie.director }}</a>
-		</div>
-	</div>
+  <div class="card text-left" style="width: 22rem;">
+    <div class="card-header">
+      <h4 class="card-heading">{{ movie.title }}</h4>
+      <div class="rating-arrow" style="display: inline-block">
+        <font-awesome-icon
+          size="2x"
+          icon="angle-up"
+          @click="updateMovieRating(1)"
+        />
+        <font-awesome-icon
+          size="2x"
+          icon="angle-down"
+          @click="updateMovieRating(-1)"
+        />
+      </div>
+    </div>
+    <div class="card-body">
+      <span class="card-text">Rating: {{ movie.rating }}</span
+      ><br />
+      <a href="#" class="card-link">Release Date: {{ movie.releaseDate }}</a
+      ><br />
+      <a href="#" class="card-link">Director: {{ fullName(movie.director) }}</a
+      ><br />
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-	name: "moviecomponent",
-	computed: {
-		
-	},
-	created(){
-		this.$store.dispatch("getMovies", );
-	}
-}
+  name: "moviecomponent",
+  props: ["movie"],
+  computed: {
+    ...mapGetters(["accessToken"])
+  },
+  methods: {
+    fullName(director) {
+      return `${director.firstName} ${director.lastName}`;
+    },
+    updateMovieRating(vote) {
+      this.$store.dispatch("updateMovieRating", {
+        id: this.movie.id,
+        vote,
+        accessToken: this.accessToken
+      });
+    }
+  }
+};
 </script>
 
 <style scoped>
-
+h4.card-heading {
+  display: inline-block;
+  text-align: left;
+}
+.rating-arrow {
+  float: right;
+}
 </style>
-
-
