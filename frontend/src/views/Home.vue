8145ae875ed73466dca5ba00cc4fbef49dd29297
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-19 19:32:35 +0100

    create homepage for the app

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,31 +1,26 @@
 <template>
   <div class="home">
-    <NavBarComponent v-bind:brand-title="brandTitle" />
-		<LoginComponent />
+    <NavBarComponent :brand-title="brandTitle" />
+    <LoginComponent />
   </div>
 </template>
 
 <script>
-
 import NavBarComponent from "@/components/NavBarComponent.vue";
 import LoginComponent from "@/components/LoginComponent.vue";
 
 export default {
   name: "home",
   components: {
-		NavBarComponent,
-		LoginComponent
-	},
-	data(){
-		return {
-			brandTitle: "Movie Review Board"
-		}
-	}
+    NavBarComponent,
+    LoginComponent
+  },
+  data() {
+    return {
+      brandTitle: "Movie Review Board"
+    };
+  }
 };
-
 </script>
 
-<style scoped>
-
-</style>
-
+<style scoped></style>
