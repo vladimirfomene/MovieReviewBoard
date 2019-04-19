f5403235b6ccad3716789122d9ecb776f6c307aa
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-19 19:41:00 +0100

    create component to handle Auth0 AuthN redirect

--- frontend/src/components/Callback.vue
+++ frontend/src/components/Callback.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="spinner-border text-primary" role="status">
-  	<span class="sr-only">Loading...</span>
+    <span class="sr-only">Loading...</span>
   </div>
 </template>
 
 <script>
+import EventBus from "./../event-bus";
+
 export default {
+  name: "callback",
   methods: {
-    handleLoginEvent(data) {
-      this.$router.push(data.state.target || "/movieboard");
+    handleLogin() {
+      this.$router.push("/movieboard");
     }
   },
   created() {
-    this.$auth.handleAuthentication();
+    this.$store.dispatch("handleAuthentication");
+    EventBus.$on("login", () => this.handleLogin());
   }
 };
 </script>
 
-
 <style scoped>
-div.spinner-border{
-	margin: 10% auto;
+div.spinner-border {
+  margin: 10% auto;
 }
 </style>
