ee51dfe6a4dac03148ba3bc3b5e1717f9cdfcb14
Vladimir Fomene <vladimirfomene@gmail.com>
2019-04-19 19:38:24 +0100

    create a navbar component for views

--- frontend/src/components/NavBarComponent.vue
+++ frontend/src/components/NavBarComponent.vue
@@ -1,43 +1,60 @@
 <template>
-	<div class="nav">
-		<nav class="navbar navbar-dark bg-primary" id="navbar">
-			<a class="navbar-brand" id="brand-title">{{ brandTitle }}</a>
-			<ul class="nav justify-content-end">
-				<li class="nav-item">
-					<router-link to="/profile" class="nav-link">Profile</router-link>
-				</li>
-				<li class="nav-item">
-					<button @click="logout">Logout</button>
-				</li>
-		</ul>
-		</nav>
-	</div>
+  <div class="nav">
+    <nav class="navbar navbar-dark bg-primary" id="navbar">
+      <router-link to="/movieboard" class="navbar-brand" id="brand-title">{{
+        brandTitle
+      }}</router-link>
+      <ul v-show="isAuthenticated" class="nav justify-content-end">
+        <li class="nav-item">
+          <button
+            class="btn btn-secondary"
+            style="display: inline-block; margin-left: 10px;"
+            @click="goToProfile"
+          >
+            Profile
+          </button>
+        </li>
+        <li class="nav-item">
+          <button
+            class="btn btn-secondary"
+            style="display: inline-block; margin-left: 10px;"
+            @click="logout"
+          >
+            Logout
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
-
 
 <script>
 export default {
-	name: "navbarcomponent",
-	props: {
-		brandTitle: String
-	},
-	methods: {
-		logout(){
-			this.$auth.logOut();
-		}
-	}
-}
+  name: "navbarcomponent",
+  props: {
+    brandTitle: String
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch("logOut");
+    },
+    goToProfile() {
+      this.$router.push({ path: "/profile" });
+    }
+  },
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters.isAuthenticated;
+    }
+  }
+};
 </script>
 
-
 <style scoped>
-#brand-title{
-	font-family: 'Roboto Slab', serif;
+#brand-title {
+  font-family: "Roboto Slab", serif;
 }
-#navbar{
-	width: 100%;
+#navbar {
+  width: 100%;
 }
 </style>
-
-
-
